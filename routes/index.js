86e7c62b6b485a8db77faa8596913ccd539c3ca8
jorgwieme <jorg.wieme@ugent.be>
2018-07-02 16:40:24 +0200

    Clean up

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const request = require("request");
 
-router.get("/", function (req, res, next) {
+router.get("/", function (req, res) {
     res.send("Miia chatbot");
+});
+
+router.post("/", function (req, res) {
+    console.log(req);
+    let dataString = '{ "query":`${res}`, "docType":"digipolis" }';
+
+    let options = {
+        url: 'https://gent.digipolis.demo.miia.technology/legal/services/v1/query',
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: dataString,
+        auth: {
+            'user': 'digipolis',
+            'pass': 'aepung5yeisu5biew3eiPohYahng5moo'
+        }
+    };
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            res.send(body);
+        } else {
+            res.send("Nothing!")
+        }
+    }
+
+    request(options, callback);
 });
 
 module.exports = router;
