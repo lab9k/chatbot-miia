b392b088ab628c4e7dc3efc707cafe19905a776f
jorgwieme <jorg.wieme@ugent.be>
2018-07-03 12:12:19 +0200

    hotfix

--- util/dialogFlowFormatter.js
+++ util/dialogFlowFormatter.js
@@ -1,18 +1,9 @@
-let card = require("../models/card");
-let carousel = require("../models/carousel");
-let facebook = require("../models/facebook");
 
 module.exports = function (miiaResponse) {
     let response;
     miiaResponse = JSON.parse(miiaResponse);
-    let cards = [];
     if (miiaResponse.hasOwnProperty("documents") && miiaResponse.documents !== null) {
-        // generate list for facebook cards
-        for (let i = 0; i < 10; i++) {
-            cards.push(card("iets", "iets", "https://lab9k.gent"))
-        }
-        // for web demo (Take the first respond(highest score)))
-        miiaResponse = miiaResponse.documents[0];
+        miiaResponse = miiaResponse.documents[0]; // Take the first respond(highest score)
         if (miiaResponse.hasOwnProperty("summary") && miiaResponse.summary !== null) {
             response = miiaResponse.summary;
         } else if (miiaResponse.hasOwnProperty("displaySummary") && miiaResponse.displaySummary !== null) {
@@ -22,7 +13,6 @@ module.exports = function (miiaResponse) {
         } else {
             response = "Geen antwoord";
         }
-
     } else {
         response = "Geen antwoord";
     }
@@ -30,8 +20,10 @@ module.exports = function (miiaResponse) {
     return {
         fulfillmentText: response,
         source: "http://miia-chatbot-gent.herokuapp.com",
-        // payload: {
-        //     facebook: facebook(carousel(cards))
-        // }
+        /*        payload: {
+                     facebook: {
+
+                     }
+                }*/
     }
 };
