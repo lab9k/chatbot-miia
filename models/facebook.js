f90e301d4ffe690ae37ae5b5ede0bf459799004f
jorgwieme <jorg.wieme@ugent.be>
2018-07-03 13:29:36 +0200

    hotfix

--- models/facebook.js
+++ models/facebook.js
@@ -1,14 +1,11 @@
 module.exports = function (carousel) {
     if (carousel.length !== 0)
         return {
-            payload: {
-                facebook: {
-                    attachment: {
-                        type: 'template',
-                        payload: carousel
-                    }
-                }
+            attachment: {
+                type: 'template',
+                payload: carousel
             }
+
         };
     else
         return {}
