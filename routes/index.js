38cd7ee02dec0d701f9d4b52cc279b000f73a3f8
Michiel Derveeuw <mderveeuw@gmail.com>
2018-07-03 11:23:50 +0200

    Create config

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const request = require("request");
 const dialogFlowFormatter = require("../util/dialogFlowFormatter");
+const config = require("../config.json");
 const miiaAPI = new MiiaAPI(
-    "https://gent.digipolis.demo.miia.technology/legal/services",
-    "digipolis",
-    "aepung5yeisu5biew3eiPohYahng5moo",
-    "digipolis"
+    config.baseURL,
+    config.username,
+    config.password,
+    config.docType
 );
 
 /**
- * Routes HTTP POST requests to root
- * TODO safety check
+ * Routes HTTP POST requests to index
  */
 router.post("/", function (req, res) {
-    miiaAPI.request(req.body.queryResult.queryText, (error, response, body) => {
+    miiaAPI.query(req.body.queryResult.queryText, (error, response, body) => {
         if (!error && response.statusCode === 200) {
             let dialogFlowResponse = dialogFlowFormatter(body);
             res.send(dialogFlowResponse);
@@ -23,7 +22,5 @@ router.post("/", function (req, res) {
         }
     });
 });
-
-// TODO all HTML METHODS(block or accept)
 
 module.exports = router;
