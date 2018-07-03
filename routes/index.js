9028aca3ad4b7bfef69336a28b031519bd949b6d
Michiel Derveeuw <mderveeuw@gmail.com>
2018-07-03 11:07:41 +0200

    Integrate wrapper in Dialogflow POST

--- routes/index.js
+++ routes/index.js
@@ -2,39 +2,26 @@ const express = require("express");
 const router = express.Router();
 const request = require("request");
 const dialogFlowFormatter = require("../util/dialogFlowFormatter");
+const miiaAPI = new MiiaAPI(
+    "https://gent.digipolis.demo.miia.technology/legal/services",
+    "digipolis",
+    "aepung5yeisu5biew3eiPohYahng5moo",
+    "digipolis"
+);
 
 /**
  * Routes HTTP POST requests to root
  * TODO safety check
  */
 router.post("/", function (req, res) {
-    let body = req.body;
-    let dataString = `{ "query":"${body.queryResult.queryText}", "docType":"digipolis" }`;
-
-    let options = {
-        url: 'https://gent.digipolis.demo.miia.technology/legal/services/v1/query',
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: dataString,
-        auth: {
-            'user': 'digipolis',
-            'pass': 'aepung5yeisu5biew3eiPohYahng5moo'
-        }
-    };
-
-    function callback(error, response, body) {
+    miiaAPI.request(req.body.queryResult.queryText, (error, response, body) => {
         if (!error && response.statusCode === 200) {
             let dialogFlowResponse = dialogFlowFormatter(body);
             res.send(dialogFlowResponse);
         } else {
             res.send(dialogFlowFormatter({}))
         }
-    }
-
-    request(options, callback);
+    });
 });
 
 // TODO all HTML METHODS(block or accept)
