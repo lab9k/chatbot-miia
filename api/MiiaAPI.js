f6ce827698a32cb99601275039759736d7459fce
Michiel Derveeuw <mderveeuw@gmail.com>
2018-07-03 11:02:03 +0200

    Create wrapper class

---
+++ api/MiiaAPI.js
@@ -0,0 +1,30 @@
+/**
+ * Wrapper klasse voor Miia Rest API
+ */
+class MiiaAPI {
+    constructor(baseURL, username, password, docType) {
+        this.baseURL = baseURL;
+        this.username = username;
+        this.password = password;
+        this.docType = docType;
+    }
+
+    query(query, callback) {
+        request({
+            url: `${this.baseURL}/v1/query`,
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: {
+                query: query,
+                docType: this.docType
+            },
+            auth: {
+                user: this.username,
+                pass: this.password
+            }
+        }, callback);
+    }
+}
