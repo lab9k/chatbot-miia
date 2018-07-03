993f3c8c8e12f41d86a878f504a39d37ed9dde9d
jorgwieme <jorg.wieme@ugent.be>
2018-07-03 11:42:44 +0200

    simple facebook test

--- util/dialogFlowFormatter.js
+++ util/dialogFlowFormatter.js
@@ -18,57 +18,11 @@ module.exports = function (miiaResponse) {
     // Dialogflow format https://dialogflow.com/docs/fulfillment
     return {
         fulfillmentText: response,
-        fulfillmentMessages: [
-            {
-                card: {
-                    title: "card title",
-                    subtitle: "card text",
-                    imageUri: "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-                    buttons: [
-                        {
-                            text: "button text",
-                            postback: "https://assistant.google.com/"
-                        }
-                    ]
-                }
-            }
-        ],
         source: "http://miia-chatbot-gent.herokuapp.com",
-        /* payload: {
-             google: {
-                 expectUserResponse: true,
-                 richResponse: {
-                     items: [
-                         {
-                             simpleResponse: {
-                                 textToSpeech: "this is a simple response"
-                             }
-                         }
-                     ]
-                 }
-             },
+        payload: {
              facebook: {
                  text: "Hello, Facebook!"
-             },
-             slack: {
-                 text: "This is a text response for Slack."
              }
-         },
-         outputContexts: [
-             {
-                 name: "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
-                 lifespanCount: 5,
-                 parameters: {
-                     param: "param value"
-                 }
-             }
-         ],
-         followupEventInput: {
-             name: "event name",
-             languageCode: "en-US",
-             parameters: {
-                 param: "param value"
-             }
-         }*/
+        }
     }
 };
