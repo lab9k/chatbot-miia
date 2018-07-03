b597fe106c20f918f9942fecd96219c7ee5860a6
jorgwieme <jorg.wieme@ugent.be>
2018-07-03 13:34:20 +0200

    hotfix

--- models/carousel.js
+++ models/carousel.js
@@ -1,10 +1,8 @@
 module.exports = function (listOfCards) {
     if (listOfCards.length !== 0)
         return {
-            payload: {
-                template_type: "generic",
-                elements: listOfCards
-            }
+            template_type: "generic",
+            elements: listOfCards
         };
     else
         return {}
