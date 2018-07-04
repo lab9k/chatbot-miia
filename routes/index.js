eedfb0e13dfeaf6777164ae166b5b2bf91ef6591
Michiel Derveeuw <mderveeuw@gmail.com>
2018-07-04 13:59:15 +0200

    Refactor POST method

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const dialogFlowFormatter = require("../util/dialogFlowFormatter");
+const Card = require("../models/Card");
+const DialogflowResponse = require("../models/DialogflowResponse");
 const MiiaAPI = require("../api/MiiaAPI");
 const miiaAPI = new MiiaAPI(
     process.env.baseURL,
@@ -15,11 +16,51 @@ const miiaAPI = new MiiaAPI(
 router.post("/", function (req, res) {
     miiaAPI.query(req.body.queryResult.queryText, (error, response, body) => {
         if (!error && response.statusCode === 200) {
-            res.send(dialogFlowFormatter(body));
+            response = JSON.parse(response);
+            let fulfillmentText;
+            let cards = [];
+            if (response.hasOwnProperty("paragraphs") && response.paragraphs !== null) {
+                // generate list for facebook cards
+                let i = 0;
+                while (i < response.paragraphs.length && cards.length < 9) {
+                    let paragraph = response.paragraphs[i];
+                    if (paragraph.matchingConcepts !== null && paragraph.matchingConcepts.length > 0) {
+                        let date = new Date(paragraph.publicationDate);
+                        cards.push(new Card(getContent(paragraph),
+                            `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
+                            paragraph.docUri));
+                    }
+                    i++;
+                }
+                // for web demo (Take the first respond(highest score)))
+                if (cards.length === 0) {
+                    fulfillmentText = "Geen antwoord gevonden";
+                } else {
+                    fulfillmentText = getContent(response.paragraphs[0])
+                }
+            } else {
+                fulfillmentText = "Geen antwoord gevonden";
+            }
+            // Dialogflow format https://dialogflow.com/docs/fulfillment
+            res.send(new DialogflowResponse(fulfillmentText, cards));
         } else {
-            res.send(dialogFlowFormatter({}))
+            res.send(new DialogflowResponse("Kon Miia API niet berijken"));
         }
     });
 });
+
+function getContent(response) {
+    let content;
+    if (response.hasOwnProperty("summary") && response.summary !== null) {
+        content = response.summary;
+    } else if (response.hasOwnProperty("displaySummary") && response.displaySummary !== null) {
+        content = response.displaySummary;
+    } else if (response.hasOwnProperty("content") && response.content !== null) {
+        content = response.content;
+    } else {
+        content = "Geen antwoord gevonden";
+    }
+    return content;
+}
 
 module.exports = router;
