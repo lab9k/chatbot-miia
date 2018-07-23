9b506a353203486a9429d7b48e5d8bba5f588327
Tom Lauwaerts <tom.lauwaerts@ugent.be>
2018-07-23 16:07:03 +0200

    Refactor code to use dialogflow-fulfillment package

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Card = require("../models/Card");
+const {WebhookClient, Card } = require("dialogflow-fulfillment");
 const DialogflowResponse = require("../models/DialogflowResponse");
 const MiiaAPI = require("../api/MiiaAPI");
 const miiaAPI = new MiiaAPI(
@@ -14,41 +14,62 @@ const miiaAPI = new MiiaAPI(
  * Routes HTTP POST requests to index
  */
 router.post("/", function (req, res) {
+    if (!req.hasOwnProperty("body") || Object.keys(req.body).length === 0) {
+        res.status(400).send("Empty body");
+    }
+
+    const agent = new WebhookClient({request: req, response: res});
+
     miiaAPI.query(req.body.queryResult.queryText, (error, response, body) => {
+        let intentMap = new Map();
         if (!error && response.statusCode === 200) {
-            response = JSON.parse(body);
-            let fulfillmentText;
-            let cards = [];
-            if (response.hasOwnProperty("paragraphs") && response.paragraphs !== null) {
-                // generate list for facebook cards
-                let i = 0;
-                while (i < response.paragraphs.length && cards.length < 9) {
-                    let paragraph = response.paragraphs[i];
-                    if (paragraph.matchingConcepts !== null && paragraph.matchingConcepts.length > 0) {
-                        let date = new Date(paragraph.publicationDate);
-                        cards.push(new Card(getContent(paragraph),
-                            `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
-                            paragraph.docUri));
-                    }
-                    i++;
-                }
-                // for web demo (Take the first respond(highest score)))
-                if (cards.length === 0) {
-                    fulfillmentText = "Geen antwoord gevonden";
-                } else {
-                    fulfillmentText = `${getContent(response.paragraphs[0])}`
-                        + `\nBekijk het verslag: ${response.paragraphs[0].docUri}`;
-                }
-            } else {
-                fulfillmentText = "Geen antwoord gevonden";
-            }
-            // Dialogflow format https://dialogflow.com/docs/fulfillment
-            res.send(new DialogflowResponse(fulfillmentText, cards));
+            intentMap.set("Default Fallback Intent", (agent) => getResponse(agent, res, body));
         } else {
-            res.send(new DialogflowResponse("Kon Miia API niet berijken"));
+            intentMap.set("Default Fallback Intent", error);
         }
+        agent.handleRequest(intentMap);
+
     });
 });
+
+function getResponse(agent, response, body) {
+    let parsedBody = JSON.parse(body);
+
+    let fulfillmentText = "Geen antwoord gevonden";
+
+    if (parsedBody.hasOwnProperty("paragraphs") && parsedBody.paragraphs !== null) {
+        // Generate list for Facebook cards
+        let i = 0;
+        let j = 0;
+        while (i < parsedBody.paragraphs.length && j < 9) {
+            let paragraph = parsedBody.paragraphs[i];
+            if (paragraph.matchingConcepts !== null && paragraph.matchingConcepts.length > 0) {
+                let date = new Date(paragraph.publicationDate);
+
+                let card = new Card(getContent(paragraph));
+                card.setText(`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`)
+                    .setButton({
+                        text: 'Bekijk het verslag',
+                        url: paragraph.docUri
+                    });
+                agent.add(card);
+                j++;
+            }
+            i++;
+        }
+        // For web demo (Take the first respond(highest score)))
+        if (j !== 0) {
+            fulfillmentText = `${getContent(parsedBody.paragraphs[0])}`
+                + `\nBekijk het verslag: ${parsedBody.paragraphs[0].docUri}`;
+        }
+    }
+    // Dialogflow format https://dialogflow.com/docs/fulfillment
+    agent.add(fulfillmentText);
+}
+
+function error(agent) {
+    agent.add(`Geen antwoord gevonden`);
+}
 
 function getContent(response) {
     let content;
