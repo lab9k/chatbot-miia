55d1ba7358f9e75bd8a547633dedd35f687c4809
Tom Lauwaerts <tom.lauwaerts@ugent.be>
2018-07-25 15:24:34 +0200

    Closes #23
    Divides answers in 3 categories. Initial bound for good answers set to 30.
    Help answer is still basic. All other responses remain the same, only split in 2 now.
    Also improves code quality slightly, more comments, more modularity, ...

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,33 @@ const miiaAPI = new MiiaAPI(
     process.env.PASSWORD,
     process.env.DOCTYPE
 );
+
+/**
+ * Documents with a score lower than LOWER_BOUND_SCORE are deemed irrelevant.
+ * @type {number}
+ */
+const LOWER_BOUND_SCORE = 5;
+
+/**
+ * Documents with a score above UPPER_BOUND_SCORE are deemed likely to be a hit.
+ * @type {number}
+ */
+const UPPER_BOUND_SCORE = 30;
+
+/**
+ * Maximum amount of cards to be shown in a carousel.
+ * @type {number}
+ */
 const MAX_CARD_AMOUNT = 10;
+
+/**
+ * Maximum amount of characters of the description of a document which may be shown in a single response message.
+ * @type {number}
+ */
 const MAX_DESCRIPTION_LENGTH = 255;
 
 /**
- * Routes HTTP POST requests to index
+ * Routes HTTP POST requests to index. It catches all fulfillment's from Dialogflow.
  */
 router.post("/", function (req, res) {
     if (!req.hasOwnProperty("body") || Object.keys(req.body).length === 0) {
@@ -36,81 +58,45 @@ router.post("/", function (req, res) {
 
 function getResponse(agent, response, body) {
     let parsedBody = JSON.parse(body);
+    let paragraphs = (parsedBody.hasOwnProperty("paragraphs")) ? parsedBody.paragraphs : [];
 
     let fulfillmentText = null;
     let cards = [];
 
-    // Get documents with highest scores
-    if (parsedBody.hasOwnProperty("documents") && parsedBody.documents !== null) {
-
-        // Take the first item (highest score, above 5) as a short text answer
-        if (parsedBody.documents.length > 0
-            && parsedBody.documents[0].hasOwnProperty("score")
-            && parsedBody.documents[0].score > 5) {
-            let doc = parsedBody.documents[0];
-            let paragraphs = [];
-            let paragraph;
-            if (parsedBody.hasOwnProperty("paragraphs")) {
-                paragraphs = getParagraphs(doc, parsedBody.paragraphs);  // Get associated paragraphs
-                paragraph = paragraphs[0];
-            }
-
-            if (paragraphs.length > 0
-                && paragraph !== null
-                && paragraph.hasOwnProperty("docUri")
-                && (getDescription(paragraph) !== null
-                    || (paragraph.hasOwnProperty("content") && paragraph.content !== null))) {
-                // Firstly try the best paragraph
-                let description = (getDescription(paragraph) !== null)
-                    ? getDescription(paragraph)
-                    : paragraph.content;
-                fulfillmentText = `${description}\nBekijk het verslag: ${paragraph.docUri}`;
-            } else if (doc.hasOwnProperty("docUri") && getDescription(doc) !== null) {
-                // Secondly try the document with link
-                fulfillmentText = `${getDescription(doc)}\nBekijk het verslag: ${doc.docUri}`;
-            } else if (getDescription(doc) !== null) {
-                // Thirdly try the document without link
-                fulfillmentText = `${getDescription(doc)}`;
-            }
-        }
-
-
-        let i = 0; // Cursor
-        let j = 0; // Card count (max 10 cards)
-        while (i < parsedBody.documents.length && j < MAX_CARD_AMOUNT) {
-            let document = parsedBody.documents[i];
-
-            // Construct a card, if this is a reasonable answer
-            let paragraphs = (parsedBody.hasOwnProperty("paragraphs"))
-                ? getParagraphs(document, parsedBody.paragraphs)
-                : [];
-            let card = getCard(document, paragraphs);
-            if (card !== null) {
-                cards.push(card);
-                j++;
-            }
-
-            i++;
-        }
-    }
-
-    // Add answers
-
-    if (fulfillmentText !== null) {
-        agent.add(fulfillmentText);
-        if (cards.length > 0) {
-            agent.add("Als dit niet het antwoord is dat u zocht kunnen onderstaande documenten u misschien helpen.");
-        }
-    } else if (cards.length > 0) {
-        agent.add("Geen antwoord gevonden, misschien kunnen onderstaande documenten je helpen?");
-    } else {
-        agent.add("Sorry ik kon geen antwoord vinden. Wilt u door een medewerken verder geholpen worden?");
+    // Basic checks
+    if (!parsedBody.hasOwnProperty("documents")
+        || parsedBody.documents === null
+        || parsedBody.documents.length <= 0) {
+        agent.add(getHelpResponse());
         return;
     }
 
-    cards.forEach(card => agent.add(card));
-    agent.add("Hopelijk is dit wat u zocht. Zo niet, wilt u dan door een medewerken verder geholpen worden?");
+    // Search for a meaningful answer
+    let highestScoring = parsedBody.documents[0];
+    if (highestScoring.hasOwnProperty("score") && highestScoring.score > UPPER_BOUND_SCORE) {
+        // If the highest scoring document has a score above UPPER_BOUND_SCORE, we only return a short response
+        fulfillmentText = getShortResponse(
+            highestScoring,
+            getParagraphs(highestScoring, paragraphs).length > 0
+                ? getParagraphs(highestScoring, paragraphs)[0]
+                : null);
+    } else {
+        // If no high scoring document was found we send a set of documents, scoring higher than LOWER_BOUND_SCORE.
+        cards = getCardResponse(document, getParagraphs(document, paragraphs));
+    }
 
+    // If no meaningful answer could be found a help response is send
+    if (fulfillmentText === null && cards.length <= 0) {
+        agent.add(getHelpResponse());
+        return;
+    }
+
+    // Send answer
+    if (fulfillmentText !== null) {
+        agent.add(fulfillmentText);
+    } else {
+        cards.forEach(card => agent.add(card));
+    }
 }
 
 function getErrorResponse(agent) {
@@ -118,17 +104,80 @@ function getErrorResponse(agent) {
 }
 
 /**
+ * Gives a response for a single document. Only displays a short paragraph or description of the document and a link to
+ * the corresponding pdf.
+ *
+ * Returns null if no meaningful message could be constructed.
+ *
+ * @param document to be represented
+ * @param paragraph from the document
+ * @returns {string|null}
+ */
+function getShortResponse(document, paragraph) {
+    // Take the first item (highest score, above 5) as a short text answer
+    let fulfillmentText;
+
+    if (paragraph !== null
+        && paragraph.hasOwnProperty("docUri")
+        && getDescription(paragraph) !== null) {
+        // Firstly try the best paragraph
+        let description = (getDescription(paragraph) !== null)
+            ? getDescription(paragraph)
+            : paragraph.content;
+        fulfillmentText = `${description}\nBekijk het verslag: ${paragraph.docUri}`;
+    } else if (document.hasOwnProperty("docUri") && getDescription(document) !== null) {
+        // Secondly try the document with link
+        fulfillmentText = `${getDescription(document)}\nBekijk het verslag: ${document.docUri}`;
+    } else {
+        fulfillmentText = `${getDescription(document)}`;
+    }
+
+    return fulfillmentText;
+}
+
+/**
+ * Gives a carousel of the best related documents in card form.
+ *
+ * @param documents
+ * @param paragraphs
+ * @returns {Array}
+ */
+function getCardResponse(documents, paragraphs) {
+    let cards = [];
+    let i = 0; // Cursor
+    let j = 0; // Card count (max 10 cards)
+    while (i < documents.length && j < MAX_CARD_AMOUNT) {
+        let document = documents[i];
+        let card = getCard(document, paragraphs);
+        if (card !== null && document.hasOwnProperty("score") && document.score > LOWER_BOUND_SCORE) {
+            cards.push(card);
+            j++;
+        }
+        i++;
+    }
+    return cards;
+}
+
+function getHelpResponse() {
+    // TODO Give some tips (refrase etc.) and give gentinfo@stad.gent for more help
+    return "Sorry ik kon geen antwoord vinden. " +
+        "U kan hopelijk verder geholpen worden op dit e-mailadres: gentinfo@stad.gent";
+}
+
+
+/**
  * Makes a card representation of a document. Returns null if no card could be made.
  *
  * @param document is the document to be represented by the card.
  * @param paragraphs is an optional parameter to improve text of card.
- * @returns {Card|*}
+ * @returns {Card|null}
  */
 function getCard(document, paragraphs) {
+    // Check if document is usable
     if (!document.hasOwnProperty("score")
         || !document.hasOwnProperty("originalURI")
-        || document.score <= 5) {
-        return;
+        || document.score <= LOWER_BOUND_SCORE) {
+        return null;
     }
 
     // Construct default card
@@ -189,7 +238,7 @@ function getDescription(item) {
     } else if (item.hasOwnProperty("content") && item.content !== null) {
         description = (item.content.length <= MAX_DESCRIPTION_LENGTH)
             ? item.content
-            : `${item.content.slice(0, MAX_DESCRIPTION_LENGTH-3)}...`;
+            : `${item.content.slice(0, MAX_DESCRIPTION_LENGTH - 3)}...`;
     }
     return description;
 }
